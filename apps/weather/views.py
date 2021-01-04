e8a4236345a040d6301caa7290bc579449149f2a
Vinh Le <lethanhvinh95@gmail.com>
2021-01-04 06:14:30 +0200

    fetch weather data from OpenWeather API

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -1,6 +1,18 @@
+import requests
 from django.shortcuts import render
 
 
 def render_view(request):
-    return render(request, "weather/index.html")
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d7ca28c2bef8ad46a170d61b4dc1f115"
+    city = "Helsinki"
+    formatted_url = url.format(city)
+    res = requests.get(formatted_url).json()
+    weather_data = {
+        "city": city,
+        "temperature": int(res["main"]["temp"]),
+        "description": res["weather"][0]["description"],
+        "icon": res["weather"][0]["icon"]
+    }
+
+    return render(request, "weather/index.html", {"weather_data": [weather_data]})
 
