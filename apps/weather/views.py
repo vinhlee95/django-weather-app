d07f13211dc6838c6ca6e52df44391ea7a69576e
Vinh Le <lethanhvinh95@gmail.com>
2021-01-08 05:23:58 +0200

    handle adding new city and get weather data for that city

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -2,9 +2,19 @@ import requests
 from django.shortcuts import render
 import logging
 from .models import City, WeatherData
+from django.http import HttpResponse
 
 
 def render_view(request):
+    if request.method == "POST":
+        city_name = request.POST.get("city_name", None)
+        if city_name:
+            City.objects.create(name=city_name)
+
+    return render(request, "weather/index.html", {"weather_data": get_weather_data()})
+
+
+def get_weather_data() -> [WeatherData]:
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d7ca28c2bef8ad46a170d61b4dc1f115"
     cities = City.objects.all()
     weather_data: [WeatherData] = []
@@ -14,7 +24,7 @@ def render_view(request):
         res = requests.get(formatted_url).json()
 
         if res["cod"] != 200:
-            logging.error("Error in getting weather data for city", extra={"city": city, "status_code": res["cod"]})
+            logging.error(f"Error in getting weather data for city {city.name}", extra={"city": city, "status_code": res["cod"]})
         else:
             weather_data.append(
                 WeatherData(
@@ -25,5 +35,4 @@ def render_view(request):
                 )
             )
 
-    return render(request, "weather/index.html", {"weather_data": weather_data})
-
+    return weather_data
